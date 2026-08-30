8d4b6e7ac2a2bda80551d62a3a506026122ac13e
Fino <finoraf01@gmail.com>
2026-08-31 02:02:33 +0700

    fix: hapus array_filter agar SSL TiDB aktif

--- config/database.php
+++ config/database.php
@@ -57,10 +57,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                1012 /* PDO::MYSQL_ATTR_SSL_CA */ => env('MYSQL_ATTR_SSL_CA', base_path('cacert.pem')),
+            'options' => extension_loaded('pdo_mysql') ? [
+                1009 /* PDO::MYSQL_ATTR_SSL_CA */ => env('MYSQL_ATTR_SSL_CA', base_path('cacert.pem')),
                 1014 /* PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT */ => env('DB_SSL_VERIFY', false),
-            ]) : [],
+            ] : [],
         ],
 
         'mariadb' => [
